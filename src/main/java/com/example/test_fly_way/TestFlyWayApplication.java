2ad147c868f239cc5da1692c36ce1228d782e8b6
Mihail Lytov <m.lytov@click-group.ru>
2024-06-20 10:31:27 +0300

    init

---
+++ src/main/java/com/example/test_fly_way/TestFlyWayApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_fly_way;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestFlyWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestFlyWayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/test_fly_way/TestFlyWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_fly_way;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestFlyWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
